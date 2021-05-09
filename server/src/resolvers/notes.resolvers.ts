d9cec73d6e97dadd6fa0c62ced24c123a724fcc3
Rasik Jain <jainrasik@gmail.com>
2021-05-09 14:34:04 +0530

    implementing update and delete

--- server/src/resolvers/notes.resolvers.ts
+++ server/src/resolvers/notes.resolvers.ts
@@ -1,29 +1,44 @@
-import {Resolver, Mutation, Arg, Query} from 'type-graphql'
-import {NotesModel, Notes } from '../models/notes.model'
+import { Resolver, Mutation, Arg, Query } from 'type-graphql'
+import { NotesModel, Notes } from '../models/notes.model'
 import { NotesInput } from './types/notes-input';
 
 @Resolver(_of => Notes)
 export class NotesResolver {
 
-    @Query(_returns => Notes, { nullable: false})
-    async getNotesById(@Arg("id") id: string)
-    {
-        
-        return await NotesModel.findById({_id: id});
-    };
+  @Query(_returns => Notes, { nullable: false })
+  async getNotesById(@Arg("id") id: string) {
 
-    @Query(() => [Notes])
-    async getAllNotes(){
-        return await NotesModel.find();
-    }
+    return await NotesModel.findById({ _id: id });
+  };
 
-    @Mutation(() => Notes)
-    async createNotes(@Arg("data"){title, description} : NotesInput) : Promise<Notes> { 
-      const notes = (await NotesModel.create({      
-        title,
-        description
-      })).save();
-      
-      return notes;
-    };
+  @Query(() => [Notes])
+  async getAllNotes() {
+    return await NotesModel.find();
+  }
+
+  @Mutation(() => Notes)
+  async createNotes(@Arg("newNotesInput") { title, description }: NotesInput): Promise<Notes> {
+    const notes = (await NotesModel.create({
+      title,
+      description
+    })).save();
+
+    return notes;
+  };
+
+  @Mutation(() => Notes)
+  async updateNotes(@Arg("newNotesInput") { id, title, description }: NotesInput): Promise<Notes> {
+    const notes = await NotesModel.findByIdAndUpdate({ _id: id }, {
+      title,
+      description
+    }, { new: true });
+
+    return notes;
+  };
+
+  @Mutation(() => Boolean)
+  async deleteNotes(@Arg("id") id: string): Promise<Boolean> {
+    await NotesModel.deleteOne({ _id: id });
+    return true;
+  };
 }
--- server/src/resolvers/types/notes-input.ts
+++ server/src/resolvers/types/notes-input.ts
@@ -1,9 +1,11 @@
 import { Field, InputType } from "type-graphql";
 import { Notes } from "../../models/notes.model";
-
 @InputType()
 export class NotesInput implements Partial<Notes>
 {
+    @Field({nullable : true})
+    id: string
+    
     @Field()
     title: string;
 
