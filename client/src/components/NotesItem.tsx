e3ba699dd3d86e0d57e98a72421132b8152a834e
Rasik Jain <jainrasik@gmail.com>
2021-05-13 17:28:36 +0530

    update cache after delete query

--- client/src/components/NotesItem.tsx
+++ client/src/components/NotesItem.tsx
@@ -1,7 +1,9 @@
 import { gql, useMutation } from '@apollo/client';
 import React from 'react';
 import { Notes } from '../models/notes';
+import { GET_NOTES_LIST } from './NotesList';
 import { deleteNotesMutationVariables } from './__generated__/deleteNotesMutation';
+import { getNotesList } from './__generated__/getNotesList';
 
 export const NotesItem = (notesDataItem: Notes) => {
   const DELETE_NOTES_MUTATION = gql`
@@ -14,24 +16,44 @@ export const NotesItem = (notesDataItem: Notes) => {
     useMutation<boolean, deleteNotesMutationVariables>(DELETE_NOTES_MUTATION);
 
   const handleDelete = (id: string) => {
-    deleteNotes({ variables: { id: id } });
+    deleteNotes({
+      variables: { id: id },
+      optimisticResponse: true,
+      update: (cache) => {
+        const existingNotes: getNotesList = cache.readQuery({ query: GET_NOTES_LIST }) ?? { notesList: [] };
+        const updatedNotesList = existingNotes.notesList.filter((t) => t.id != id);
+        cache.writeQuery({
+          query: GET_NOTES_LIST,
+          data: {
+            notesList: updatedNotesList,
+          },
+        });
+      },
+    });
   };
+
+  console.log(notesDataItem.description);
   return (
     <div>
-      <div className="card" style={{ width: '30rem', backgroundColor: notesDataItem.backgroundColor || '#fff' }}>
-        <div className="card-header">
-          <h5 className="card-title">{notesDataItem.title}</h5>
+      <div className="card" style={{ width: '30rem' }}>
+        <div className="card-body" style={{ backgroundColor: notesDataItem.backgroundColor || '#fff' }}>
+          <h5 className="card-title text-center">{notesDataItem.title}</h5>
+          <p className="card-text">
+            <div className="display-linebreak">{notesDataItem.description}</div>
+          </p>
         </div>
-        <div className="card-body">
-          <p className="card-text">{notesDataItem.description}</p>
-          <button type="button" className="btn btn-danger btn-sm mx-2" onClick={() => handleDelete(notesDataItem.id)}>
+        <div className="m-2">
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm mx-2"
+            onClick={() => handleDelete(notesDataItem.id)}
+          >
             Delete
           </button>
-          <button type="button" className="btn btn-primary btn-sm mx-2" onClick={() => {}}>
+          <button type="button" className="btn btn-outline-primary btn-sm mx-2" onClick={() => {}}>
             Edit
           </button>
         </div>
-        <div className="card-footer text-muted"></div>
       </div>
     </div>
   );
